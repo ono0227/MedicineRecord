c24706894e485258c6398f93f4e4906e7a19170c
poh <pow@PohMacBook-Air.local>
2025-03-24 22:54:12 +0900

    add:新規ファイル追加

---
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::UsersController < Api::V1::AliasController
+  before_action :authenticate_user!
+
+  def currentuser
+    @user = current_user
+    render json: { data: @user, status: :success }
+  end
+end
+  
