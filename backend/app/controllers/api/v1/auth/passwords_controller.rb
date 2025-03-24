11bd91456f4cf13433c8d8a300b6bf50d08d751a
poh <pow@PohMacBook-Air.local>
2025-03-24 22:37:08 +0900

    fix:バグ修正

--- backend/app/controllers/api/v1/auth/passwords_controller.rb
+++ backend/app/controllers/api/v1/auth/passwords_controller.rb
@@ -1,9 +1,16 @@
 class Api::V1::Auth::PasswordsController < DeviseTokenAuth::PasswordsController
-    before_action: :ensure_not_guest_user, only: :create
+    before_action :set_resource, only: :create
+    before_action :ensure_not_guest_user, only: :create
 
     def ensure_not_guest_user
-        if @email.downcase == "guest@example.com"
+        if @resource && @resource.email.downcase == "guest@example.com"
             render json: { error: 'ゲストユーザーはアカウント情報を変更できません。' }, status: :forbidden
         end
+    end
+
+    private
+
+    def set_resource
+        @resource = resource_class.find_by(email: resource_params[:email])
     end
 end
