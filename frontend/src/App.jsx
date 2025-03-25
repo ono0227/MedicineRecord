d7ccab48bd7dbf633ea4b57509f62ed8ae70ebee
poh <pow@PohMacBook-Air.local>
2025-03-25 23:30:06 +0900

    update:機能修正

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Router from './Router'
-import Header from './components/Header/Header';
+import { Header } from './components/Header';
+import "./assets/reset.css"
+import "./assets/style.css"
 
 const App = () => {
   return (
     <>
       <Header />
-      <main>
+      <main className="c-main">
         <Router />
       </main>
     </>
