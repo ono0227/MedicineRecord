b35e87e62d23cf6e0b1e574c56f4118bbc737b64
poh <pow@PohMacBook-Air.local>
2025-04-14 20:28:24 +0900

    remove：削除

--- backend/app/controllers/api/v1/auth/sessions_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
-    private
-
-    # Only allow a list of trusted parameters through.
-    def resource_params
-      params.require(:session).permit(:email, :password)
-    end
-end
