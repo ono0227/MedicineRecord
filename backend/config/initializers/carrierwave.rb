568155b2995618b6b0fba2233e90797dfc17f162
poh <pow@PohMacBook-Air.local>
2025-04-24 17:58:08 +0900

    add：新規（ファイル）機能追加

---
+++ backend/config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_provider = 'AWS'
+    config.fog_credentials = {
+        provider:              'AWS',
+        region:                ENV['AWS_REGION'],
+    }
+    config.fog_directory  = ENV['AWS_S3_BUCKET_NAME']
+    config.fog_public     = false
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.days.to_i}" }
+end
